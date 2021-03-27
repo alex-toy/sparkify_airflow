56fee9a92fbacdd207232ac797e872d2d01dd92a
alessio rea <alexei.80@hotmail.fr>
2021-03-27 09:19:29 +0100

    progress dag parameters

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -14,11 +14,27 @@ default_args = {
     'start_date': datetime(2019, 1, 12),
 }
 
-dag = DAG('udac_example_dag',
-          default_args=default_args,
-          description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
-        )
+default_args = {
+    'owner': 'udacity',
+    'start_date': datetime(2018, 11, 1),
+    'end_date': datetime(2018, 11, 3),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'email_on_retry': False
+}
+
+# DAG
+dag = DAG(
+    'udac_example_dag',
+    default_args=default_args,
+    description='Load and transform data in Redshift with Airflow',
+    schedule_interval='0 * * * *',
+    catchup=True,
+    max_active_runs=1
+)
+
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
