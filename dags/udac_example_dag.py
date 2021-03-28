40e4eb36d4320495c57af04fc06ec63f7225366f
alessio rea <alexei.80@hotmail.fr>
2021-03-28 13:05:19 +0200

    progress operators

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -88,22 +88,31 @@ load_user_dimension_table = LoadDimensionOperator(
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    query=SqlQueries.song_table_insert
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    query=SqlQueries.artist_table_insert
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    query=SqlQueries.time_table_insert
 )
 
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="",
+    column="",
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
