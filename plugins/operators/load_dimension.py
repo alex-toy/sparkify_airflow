65ab403943edfd0ed2ac0d721e20483a0546e426
alessio rea <alexei.80@hotmail.fr>
2021-04-01 09:41:54 +0200

    LoadDimensionOperator

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -5,27 +5,35 @@ from airflow.utils.decorators import apply_defaults
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
+    truncate_sql = f"""
+        TRUNCATE TABLE {};
+    """
 
     @apply_defaults
     def __init__(self,
                  redshift_conn_id="",
+                 table="",
                  query="",
                  append=False,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
+        self.table = table
         self.query = query
         self.append = append
 
     def execute(self, context):
         redshift = PostgresHook(self.redshift_conn_id)
         if self.append == True:
-            sql_statement = self.query
-            redshift.run(sql_statement)
+            sql_statement = truncate_sql.format(self.table) + self.query
         else:
             sql_statement = self.query
-            redshift.run(sql_statement)
-            
+        
+        redshift.run(sql_statement)
         self.log.info(f"LoadDimensionOperator : {self.query}")
 
+        
+        
+        
+        
