c8288d6a35f2ade934f3b3db3ab06b370d4fec43
alessio rea <alexei.80@hotmail.fr>
2021-04-02 08:42:01 +0200

    DataQualityOperator

--- dags/global_dag.py
+++ dags/global_dag.py
@@ -25,7 +25,6 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
     'catchup': False,
     'email_on_retry': False,
-    'null_check' : {'check_sql': """SELECT COUNT(*) FROM {} WHERE {} IS NULL""", 'expected_result':0}
 }
 
 # DAG
