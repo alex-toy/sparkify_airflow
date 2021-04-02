f505046ef3da3db45b5a7baa6f94e965cba19836
alessio rea <alexei.80@hotmail.fr>
2021-04-02 08:24:50 +0200

    DataQualityOperator

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -9,19 +9,27 @@ class DataQualityOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  redshift_conn_id="",
-                 table="",
-                 column="",
+                 query="",
+                 table=[],
+                 columns=[],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
-        self.table = table
-        self.column = column
+        self.query = query
+        self.tables = tables
+        self.columns = columns
 
     def execute(self, context):
         redshift_hook = PostgresHook(self.redshift_conn_id)
-        query = f"SELECT COUNT({self.column}) FROM {self.table} WHERE {self.column} IS NULL"
+        check_query = self.query.format(self.table, self.column)
+        #query = f"SELECT COUNT({self.column}) FROM {self.table} WHERE {self.column} IS NULL"
         records = redshift_hook.get_records(query)
+        self.log.info(f"records : {records}.")
         if len(records) < 1 or len(records[0]) < 1 :
             raise ValueError(f"data quality check failed : {self.column} contains nulls.")
         self.log.info(f"Data Quality check on table {self.table} passed.")
+        
+        
+        
+        
